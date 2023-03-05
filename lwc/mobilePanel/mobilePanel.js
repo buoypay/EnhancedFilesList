2062ebbadd41972aa75cff86c57273a1aa328da0
Martin Cichowski <mcichowski2015@alumni.hult.edu>
2023-03-05 12:22:13 +0100

    fix icon size

--- lwc/mobilePanel/mobilePanel.js
+++ lwc/mobilePanel/mobilePanel.js
@@ -44,7 +44,7 @@ export default class MobilePanel extends NavigationMixin(LightningElement) {
     if(!this.childFieldName || this.childFieldName == '') {
       return this.recordId
     }
-    return this.record.data ? getFieldValue(this.record.data, this.childFieldName) : '';
+    return this.record.data ? this.record.data.fields[this.childFieldName].value : '';
   }
   
 
